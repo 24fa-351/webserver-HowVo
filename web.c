b52c6a6a3a33799729b4ff19038e1a6b731c0aaf
Hao Doan Anh Vo <146135155+HowVo@users.noreply.github.com>
2024-11-20 00:34:01 -0800

    Done!

--- web.c
+++ web.c
@@ -16,26 +16,76 @@ pthread_mutex_t mutex;
 
 void read_http_request(int client_fd, char *request)
 {
-    int bytes_read = read(client_fd, request, BUFFER_SIZE - 1);
-    if (bytes_read < 0)
-    {
-        perror("Read failed");
-        return;
-    }
+    int bytes_read;
+    int len_read = 0;
+    char buffer[BUFFER_SIZE];
+    int found_crlfcrlf = 0;
+    int is_client_disconnected = 0;
 
-    else if (bytes_read == 0)
+    while (!found_crlfcrlf)
     {
-        printf("Client disconnected\n");
-        return;
+        bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
+        if (bytes_read < 0)
+        {
+            perror("Read failed");
+            return;
+        }
+
+        if (bytes_read == 0)
+        {
+            if (!is_client_disconnected)
+            {
+                printf("Client disconnected\n");
+                is_client_disconnected = 1;
+            }
+            return;
+        }
+
+        pthread_mutex_lock(&mutex);
+        total_received_bytes += bytes_read;
+        pthread_mutex_unlock(&mutex);
+
+        memcpy(request + len_read, buffer, bytes_read);
+        len_read += bytes_read;
+        request[len_read] = '\0';
+
+        if (len_read >= 4 && strstr(request, "\r\n\r\n") != NULL)
+        {
+            found_crlfcrlf = 1;
+        }
     }
-    request[bytes_read] = '\0';
 }
 
 void send_http_response(int client_fd, char *message)
 {
+    const char *content_type = "text/html";
+    if (strstr(message, ".png") != NULL)
+    {
+        content_type = "image/png";
+    }
+    if (strstr(message, ".css") != NULL)
+    {
+        content_type = "text/css";
+    }
+    if (strstr(message, ".js") != NULL)
+    {
+        content_type = "application/javascript";
+    }
+    if (strstr(message, ".jpg") != NULL)
+    {
+        content_type = "image/jpeg";
+    }
+    if (strstr(message, ".gif") != NULL)
+    {
+        content_type = "image/gif";
+    }
+    if (strstr(message, ".json") != NULL)
+    {
+        content_type = "application/json";
+    }
     const char *default_msg = "HTTP/1.1 200 OK\n"
                               "Server: webserver-c\n"
-                              "Content-type: text/html\n"
+                              "Content-type: %s\n"
                               "Content-Length: %d\n"
                               "Connection: Keep-Alive\n"
                               "\n%s";
@@ -43,7 +93,7 @@ void send_http_response(int client_fd, char *message)
     int response_buffer_size = strlen(default_msg) + 100 + strlen(message);
     char *response = malloc(response_buffer_size);
 
-    snprintf(response, response_buffer_size, default_msg, (int)strlen(message), message);
+    snprintf(response, response_buffer_size, default_msg, content_type, (int)strlen(message), message);
     total_sent_bytes += strlen(response);
     write(client_fd, response, strlen(response));
     free(response);
@@ -65,16 +115,26 @@ void handle_static_request(int client_fd, char *request)
     if (result < 1 || result == EOF)
     {
         send_error_response(client_fd);
+        return;
     }
 
-    int fd = open(file_path, O_RDONLY);
-    char file_contents[BUFFER_SIZE];
-    int bytes_read;
-    while ((bytes_read = read(fd, file_contents, sizeof(file_contents))) > 0)
+    char full_path[BUFFER_SIZE] = "./static/";
+    strcat(full_path, file_path);
+
+    int fd = open(full_path, O_RDONLY);
+    if (fd == -1)
     {
-        write(client_fd, file_contents, bytes_read);
+        send_error_response(client_fd);
+        return;
     }
 
+    char buffer[BUFFER_SIZE];
+    int bytes_read;
+    while ((bytes_read = read(fd, buffer, sizeof(buffer))) > 0)
+    {
+        write(client_fd, buffer, bytes_read);
+    }
+    
     pthread_mutex_lock(&mutex);
     total_sent_bytes += bytes_read;
     pthread_mutex_unlock(&mutex);
@@ -85,16 +145,17 @@ void handle_static_request(int client_fd, char *request)
 void handle_stats_request(int client_fd)
 {
     char stats[BUFFER_SIZE];
-    snprintf(stats, sizeof(stats),
-             "<html><body><h1>Server Statistics</h1>"
-             "<p>Total requests: %llu</p>"
-             "<p>Total received bytes: %llu</p>"
-             "<p>Total sent bytes: %llu</p></body></html>",
-             total_requests, total_received_bytes, total_sent_bytes);
 
     pthread_mutex_lock(&mutex);
     total_sent_bytes += strlen(stats);
     pthread_mutex_unlock(&mutex);
+
+    snprintf(stats, sizeof(stats),
+             "<html><body><h1>Server Statistics</h1>"
+             "<p>Total requests: %llu</p>"
+             "<p>Total received bytes: %llu</p>"
+             "<p>Total sent bytes: %llu</p></body></html>\n",
+             total_requests, total_received_bytes, total_sent_bytes);
 
     send_http_response(client_fd, stats);
 }
@@ -119,54 +180,63 @@ void handle_calc_request(int client_fd, char *request)
     send_http_response(client_fd, response_body);
 }
 
-void *handle_client(int *arg)
+void *handle_client(void *arg)
 {
-    int bytes_read, client_fd;
-    char request[BUFFER_SIZE];
-
-    client_fd = *(arg);
+    int client_fd = *(int *)arg;
     free(arg);
 
-    read_http_request(client_fd, request);
+    char request[BUFFER_SIZE];
 
-    if (strstr(request, "GET /static/") == request)
+    while (1)
     {
-        handle_static_request(client_fd, request);
-    }
-    else if (strstr(request, "GET /stats") == request)
-    {
-        handle_stats_request(client_fd);
-    }
-    else if (strstr(request, "GET /calc/") == request)
-    {
-        handle_calc_request(client_fd, request);
-    }
-    else
-    {
-        send_error_response(client_fd);
-    }
 
-    pthread_mutex_lock(&mutex);
-    total_requests++;
-    pthread_mutex_unlock(&mutex);
+        pthread_mutex_lock(&mutex);
+        total_requests++;
+        pthread_mutex_unlock(&mutex);
+
+        memset(request, 0, sizeof(request));
+
+        read_http_request(client_fd, request);
+
+        if (strlen(request) == 0)
+        {
+            break;
+        }
+
+        if (strstr(request, "GET /static/") == request)
+        {
+            handle_static_request(client_fd, request);
+        }
+        else if (strstr(request, "GET /stats") == request)
+        {
+            handle_stats_request(client_fd);
+        }
+        else if (strstr(request, "GET /calc/") == request)
+        {
+            handle_calc_request(client_fd, request);
+        }
+        else
+        {
+            send_error_response(client_fd);
+        }
+    }
 
     close(client_fd);
+    return NULL;
 }
 
 int main(int argc, char **argv)
 {
-    if (argc != 3 || strcmp(argv[1], "-p") != 0)
-    {
-        fprintf(stderr, "Usage: %s -p <port>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
     int server_fd, *client_fd;
+    int port = DEFAULT_PORT;
     struct sockaddr_in addr;
     pthread_t thread_id;
     pthread_mutex_init(&mutex, NULL);
 
-    int port = atoi(argv[2]);
+    if (argc > 2 && strcmp(argv[1], "-p") == 0)
+    {
+        port = atoi(argv[2]);
+    }
 
     if (port <= 0)
     {
@@ -185,13 +255,13 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    listen(server_fd, 1);
+    listen(server_fd, 5);
 
     while (1)
     {
         client_fd = malloc(sizeof(int));
         *client_fd = accept(server_fd, NULL, NULL);
-        printf("Client connected\n");
+        printf("Client %d connected\n", *client_fd);
         if (pthread_create(&thread_id, NULL, (void *)handle_client, client_fd) != 0)
         {
             perror("Thread creation failed");
